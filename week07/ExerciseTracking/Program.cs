0ebc8787ff3834d9c9cf0681abdc70324b77bad0
josuemendozap <josue.mendozap@gmail.com>
2025-12-09 00:41:18 -0600

    W07 Assignment: Exercise Tracking Program

--- week07/ExerciseTracking/Program.cs
+++ week07/ExerciseTracking/Program.cs
@@ -5,5 +5,16 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello World! This is the ExerciseTracking Project.");
+
+        List<Activity> activities = new List<Activity>();
+
+        activities.Add(new Running("08 Dic 2025", 30, 3.0));
+        activities.Add(new Cycling("09 Dic 2025", 45, 12.0));
+        activities.Add(new Swimming("10 Dic 2025", 40, 20));
+
+        foreach (Activity activity in activities)
+        {
+            Console.WriteLine(activity.GetSummary());
+        }
     }
 }
